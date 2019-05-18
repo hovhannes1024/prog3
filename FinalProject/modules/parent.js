661f4dee4a7928461610f92ab4bb5b40acdd1e46
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2019-05-18 12:22:51 +0400

    final

---
+++ FinalProject/modules/parent.js
@@ -0,0 +1,20 @@
+module.exports = class Parent {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+        this.directions = [];
+    }
+    newDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+}
---
+++ FinalProject2/random_matrix.js
@@ -0,0 +1,23 @@
+var bardz = 30;
+var layn = 100;
+var grassCount = 120;
+var eatGrassCount = 10;
+var matrix = [];
+
+
+//Լցնենք մատրիցան 0-ներով
+for (var i = 0; i < bardz; i++) {
+    matrix.push([]);
+    for (var j = 0; j < layn; j++) {
+        matrix[i].push(0);
+    }
+}
+var n = 0;
+while (n < grassCount) {
+    var x = Math.floor(Math.random(0, layn));
+    var y = Math.floor(Math.random(0, bardz));
+    if (matrix[y][x] == 0) {
+        matrix[y][x] = 1;
+        n++;
+    }
+}
